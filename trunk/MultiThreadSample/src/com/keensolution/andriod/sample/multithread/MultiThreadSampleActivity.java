da416b4601e21c085f2a1d40dfaa73ac4902d1e9
ouyuehu@gmail.com <ouyuehu@gmail.com@f5f2a49c-ddfe-3dce-2a0a-722a8b300359>
2012-02-29 02:17:12 +0000

    Initial import.

---
+++ trunk/MultiThreadSample/src/com/keensolution/andriod/sample/multithread/MultiThreadSampleActivity.java
@@ -0,0 +1,13 @@
+package com.keensolution.andriod.sample.multithread;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MultiThreadSampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
